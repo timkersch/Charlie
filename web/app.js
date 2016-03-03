b2c4150f9b3258216f7ba565ce35893e6ebd89c4
Erik Nordmark <e.n.nordmark@gmail.com>
2016-03-03 15:20:28 +0100

    Added controller

--- web/app.js
+++ web/app.js
@@ -1,14 +1,23 @@
 'use strict';
 
 var charlieApp = angular.module('charlieApp', [
+	'ngRoute',
     'ngMaterial',
 	'signupService'
 ]);
 
 charlieApp.config(['$routeProvider',
 	function($routeProvider){
-		$routeProvider.when('/', {
-			templateUrl: '.html',
+		$routeProvider.
+        when('/', {
+			templateUrl: 'index.html',
 			controller: 'exempelController' 	
-		});	
+		}).
+        when('/signup', {
+            templateUrl: '/partials/signupview.html',
+            controller: 'signupController'
+        }).
+        otherwise({
+            redirectTo: '/index.html'
+        });
 	}]);
