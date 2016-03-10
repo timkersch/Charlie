797e7a06f7102d4f82708d9a6fadbb931f54653f
Joakim Berntsson <jcberntsson@gmail.com>
2016-03-10 09:35:20 +0100

    added it again

---
+++ src/main/java/core/SpotifyCredentials.java
@@ -0,0 +1,14 @@
+package core;
+
+/**
+ * Created by: Tim Kerschbaumer
+ * Project: SpotHoot
+ * Date: 2016-03-03
+ * Time: 22:25
+ */
+
+public class SpotifyCredentials {
+    public static final String clientID = "ac1b4fadbe0e4e8bad11376ef55c636d";
+    public static final String clientSecret = "d514c20cffdf4966a08b403adbf7271f";
+    public static final String redirectURI = "http://localhost:8080/SpotHoot/callback.html";
+}
