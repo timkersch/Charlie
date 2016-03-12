064e56555a9a96075334486efa1b33da4432de8c
timkersch <tim.kersch@gmail.com>
2016-03-12 14:15:36 +0100

    Updaed code for testing SpotifyService

--- src/main/java/core/SpotifyService.java
+++ src/main/java/core/SpotifyService.java
@@ -1,11 +1,15 @@
 package core;
 
+import com.google.common.util.concurrent.FutureCallback;
+import com.google.common.util.concurrent.Futures;
+import com.google.common.util.concurrent.SettableFuture;
 import com.wrapper.spotify.Api;
 import com.wrapper.spotify.exceptions.WebApiException;
 import com.wrapper.spotify.methods.AddTrackToPlaylistRequest;
 import com.wrapper.spotify.methods.PlaylistCreationRequest;
 import com.wrapper.spotify.methods.PlaylistTracksRequest;
 import com.wrapper.spotify.methods.UserPlaylistsRequest;
+import com.wrapper.spotify.methods.authentication.ClientCredentialsGrantRequest;
 import com.wrapper.spotify.models.*;
 
 import java.io.IOException;
@@ -29,6 +33,32 @@ public class SpotifyService {
 			.clientSecret(SpotifyCredentials.clientSecret)
 			.redirectURI(SpotifyCredentials.redirectURI)
 			.build();
+
+	public void initTests() {
+		final ClientCredentialsGrantRequest reqTest = api.clientCredentialsGrant().build();
+		final SettableFuture<ClientCredentials> responseFuture = reqTest.getAsync();
+		/* Add callbacks to handle success and failure */
+		Futures.addCallback(responseFuture, new FutureCallback<ClientCredentials>() {
+			@Override
+			public void onSuccess(ClientCredentials clientCredentials) {
+                /* The tokens were retrieved successfully! */
+				System.out.println("Successfully retrieved an access token! " + clientCredentials.getAccessToken());
+				System.out.println("The access token expires in " + clientCredentials.getExpiresIn() + " seconds");
+
+                /* Set access token on the Api object so that it's used going forward */
+				api.setAccessToken(clientCredentials.getAccessToken());
+
+            /* Please note that this flow does not return a refresh token.
+            * That's only for the Authorization code flow */
+			}
+
+			@Override
+			public void onFailure(Throwable throwable) {
+            /* An error occurred while getting the access token. This is probably caused by the client id or
+            * client secret is invalid. */
+			}
+		});
+	}
 
 
 	public String getAuthorizeURL() {
