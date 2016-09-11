7e407f13d365423753dc34957f405ef9a2b848ae
timkersch <tim.kersch@gmail.com>
2016-09-11 14:31:55 +0100

    Changed to route handling to uiRouter

--- public/js/app.js
+++ public/js/app.js
@@ -1,23 +1,19 @@
 'use strict';
 
-let charlieController = angular.module("charlieController", [
+let charlieController = angular.module('charlieController', [
     'ngRoute',
     'charlieService',
     'ngMaterial'
 ]);
 
-let charlieApp = angular.module("charlieApp", [
+let charlieApp = angular.module('charlieApp', [
     'ngMessages',
     'charlieController',
     'ui.router',
-    'routeStyles'
+    'uiRouterStyles'
 ]);
 
 charlieApp.config(['$stateProvider', '$urlRouterProvider', '$locationProvider', function ($stateProvider, $urlRouterProvider, $locationProvider) {
-
-    $locationProvider.html5Mode(true);
-
-    $urlRouterProvider.otherwise('/');
 
     $stateProvider
 
@@ -25,61 +21,95 @@ charlieApp.config(['$stateProvider', '$urlRouterProvider', '$locationProvider', 
             url: '/',
             templateUrl: '../views/home.html',
             controller: 'homeController',
-            css: '../css/partials/home.css'
+            data: {
+                css: ['../css/partials/home.css']
+            }
         })
 
         .state('lobby', {
             url: '/lobby',
             templateUrl: '../views/lobby.html',
             controller: 'lobbyController',
-            css: '../css/partials/lobby.css'
+            data: {
+                css: ['../css/partials/lobby.css']
+            }
         })
 
         .state('join', {
             url: '/join',
             templateUrl: '../views/join.html',
             controller: 'joinController',
-            css: '../css/partials/join.css'
+            data: {
+                css: ['../css/partials/join.css']
+            }
         })
 
         .state('create', {
             url: '/create',
             templateUrl: '../views/create.html',
             controller: 'createController',
-            css: '../css/partials/create.css'
+            data: {
+                css: ['../css/partials/create.css']
+            }
         })
 
         .state('create2', {
             url: '/create2',
             templateUrl: '../views/create2.html',
             controller: 'createNavigationController',
-            css: '../css/partials/create2.css'
+            data: {
+                css: ['../css/partials/create2.css']
+            }
         })
 
-        .state('create2.createPlaylist', {
-            url: '/playlist',
-            templateUrl: '../views/createFromPlaylist'
+        .state('create2.createFromPlaylist', {
+            url: '/simple',
+            templateUrl: '../views/createFromPlaylist.html',
+        })
+
+        .state('create2.generateFromPlaylist', {
+            url: '/generate',
+            templateUrl: '../views/generateFromPlaylist.html',
+        })
+
+        .state('create2.createFromFeatured', {
+            url: '/featured',
+            templateUrl: '../views/createFromFeatured.html',
+        })
+
+        .state('create2.createFromPopular', {
+            url: '/popular',
+            templateUrl: '../views/createFromPopular.html',
         })
 
         .state('profile', {
             url: '/profile',
             templateUrl: '../views/profile.html',
             controller: 'profileController',
-            css: '../css/partials/profile.css'
+            data: {
+                css: ['../css/partials/profile.css']
+            }
         })
 
         .state('scoreboard', {
             url: '/scoreboard',
             templateUrl: '../views/scoreboard.html',
             controller: 'scoreboardController',
-            css: '../css/partials/scoreboard.css'
+            data: {
+                css: ['../css/partials/scoreboard.css']
+            }
         })
 
         .state('question', {
             url: '/question',
             templateUrl: '../views/question.html',
             controller: 'questionController',
-            css: '../css/partials/question.css'
+            data: {
+                css: ['../css/partials/question.css']
+            }
         });
+
+    $urlRouterProvider.otherwise('/');
+    $locationProvider.html5Mode(true);
 
 }]);
